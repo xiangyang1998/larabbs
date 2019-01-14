1f4a891ddeffc8e0f9d4280e6423243c1ee63523
xy <827544120@qq.com>
2019-01-14 15:41:11 +0800

    Prevent XSS injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
+use App\Models\User;
+use App\Observers\TopicObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +18,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Carbon\Carbon::setLocale('zh');
+        User::observe(UserObserver::class);
+
+        Topic::observe(TopicObserver::class);
     }
 
     /**
